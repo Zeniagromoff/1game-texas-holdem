f9fc6b7fbf9402a0630556e51886439935b7ef48
鄭脈龍 <hausenism@hotmail.com>
2016-07-04 16:27:07 +0800

    feat(rule): add check for Straight

--- src/rule.js
+++ src/rule.js
@@ -1,28 +1,61 @@
-function count(showhand, board) {
-    var arr = board.concat(showhand);
-    var suits = new Map(), suit;
-    for (var i = 0; i < arr.length; i++) {
-        var card = arr[i];
-        if (!suits.has(card.suit)) {
-            if (suits.size == 3) break;
-            suits.set(card.suit, []);
-        }
-        var suited = suits.get(card.suit);
-        suited.push(i);
-        if (suited.length == 5) { suit = card.suit }
+'use strict'
+const straight = (1 << 5) + (1 << 4) + (1 << 3) + (1 << 2) + (1 << 1);
+
+function _count(input) {
+    var res = new Object(), sts = res.suits = new Map(), rks = res.ranks = new Map();
+    res.opps = 7 - input.length;
+    for (var i = 0; i < input.length; i++) {
+        var card = input[i];
+        var suit = card.suit, rValue = card.rank.value;
+        if (sts.has(suit)) { sts.get(suit).push(rValue); }
+        else { sts.set(suit, [rValue]); }
+        if (rks.has(rValue)) { rks.set(rValue, rks.get(rValue) + 1); }
+        else { rks.set(rValue, 1); }
     }
-    if (suit !== undefined) {
-        var r = countRanks(suited.map(function (i) { return arr[i]; }));
-        return {
-            type: 'flush',
-            suit: suit,
-            suited: suited,
-        }
-    }
-    return;
+    return res;
 }
 
-function countRanks(arr) {
+function _countStraightHigh(rankValueArr) {
+    var c = 0, s = 0, res = new Object(), f, h = 0;
+    for (var i = 0; i < rankValueArr.length; i++) {
+        var value = rankValueArr[i];
+        s += (1 << value);
+        if (value == 14) s += (1 << 1);
+        if (h < value) h = value;
+    }
+    while (s >= straight) {
+        if ((s & straight) == straight) { f = c; }
+        ++c; s = s >> 1;
+    }
+    res.strHigh = f ? 5 + f : f;
+    res.high = h;
+    return res;
+}
+
+function count(showhand, board) {
+    var input = showhand.concat(board);
+    var res = _count(input);
+    res.suits.forEach(function (rankValueArr, suit, map) {
+        if (rankValueArr.length >= 5) {
+            res.suit = suit;
+            var r = _countStraightHigh(rankValueArr);
+            if (r.strHigh) {
+                res.type = 'Straight Flush';
+                res.high = r.strHigh;
+            } else {
+                res.type = 'Flush';
+                res.high = r.high;
+            }
+        }
+    })
+    if (!res.type) {
+        var r = _countStraightHigh(Array.from(res.ranks.keys()));
+        if (r.strHigh) {
+            res.type = 'Straight';
+            res.high = r.strHigh;
+        }
+    }
+    return res;
 }
 
 module.exports = { count: count };
--- test/rule.test.js
+++ test/rule.test.js
@@ -1,42 +1,44 @@
 import {expect} from 'chai';
 import {rule, poker} from '../src/index';
-const count = rule.count, $ = poker.Card.from;
+const count = rule.count, $ = poker.Card.from, R = poker.Rank.from;
 
 describe('rule', function () {
 
     describe('count', function () {
-        it('should be a Flush', function () {
-            var showhand = [
-                $('Diamonds', 'Seven'),
-                $('Diamonds', 'Ace')
-            ];
-            var board = [
-                $('Diamonds', 'Eight'),
-                $('Hearts', 'Four'),
-                $('Diamonds', 'Nine'),
-                $('Diamonds', 'Two'),
-                $('Diamonds', 'Four')
-            ];
+
+        it('should be a Straight Flush with Ten High', function () {
+            var showhand = [$('Diamonds', 'Seven'), $('Diamonds', 'Ten')];
+            var board = [$('Diamonds', 'Eight'), $('Hearts', 'Four'), $('Diamonds', 'Nine'), $('Clubs', 'Two'), $('Diamonds', 'Six')];
             var r = count(showhand, board);
-            expect(r).to.have.property('type', 'flush');
+            expect(r).to.have.property('type', 'Straight Flush');
             expect(r).to.have.property('suit', 'Diamonds');
-            expect(r.suited).to.be.instanceof(Array).to.eql([0, 2, 3, 4, 5, 6]);
+            expect(r).to.have.property('high', R('Ten').value);
         })
 
-        it('should not be undefined for now', function () {
-            var showhand = [
-                $('Spades', 'Seven'),
-                $('Clubs', 'Eight')
-            ];
-            var board = [
-                $('Hearts', 'Four'),
-                $('Diamonds', 'Nine'),
-                $('Spades', 'Eight'),
-                $('Hearts', 'Nine'),
-                $('Diamonds', 'Four')
-            ];
+        it('should be a Flush with Ace High', function () {
+            var showhand = [$('Diamonds', 'Seven'), $('Diamonds', 'Ace')];
+            var board = [$('Diamonds', 'Eight'), $('Hearts', 'Four'), $('Diamonds', 'Nine'), $('Diamonds', 'Two'), $('Diamonds', 'Four')];
             var r = count(showhand, board);
-            expect(r).to.be.undefined;
+            expect(r).to.have.property('type', 'Flush');
+            expect(r).to.have.property('suit', 'Diamonds');
+            expect(r).to.have.property('high', R('Ace').value);
+        })
+
+        it('should be a Straight with Jack High', function () {
+            var showhand = [$('Spades', 'Seven'), $('Clubs', 'Eight')];
+            var board = [$('Hearts', 'Four'), $('Diamonds', 'Nine'), $('Spades', 'Ten'), $('Hearts', 'Jack'), $('Diamonds', 'Four')];
+            var r = count(showhand, board);
+            expect(r).to.have.property('type', 'Straight');
+            expect(r).to.have.property('high', R('Jack').value);
+        })
+
+        it('should be implemented later', function () {
+            var showhand = [$('Spades', 'Seven'), $('Clubs', 'Eight')];
+            var board = [$('Hearts', 'Four'), $('Diamonds', 'Nine'), $('Spades', 'Eight'), $('Hearts', 'Nine'), $('Diamonds', 'Four')];
+            var r = count(showhand, board);
+            expect(r.suits).to.be.instanceof(Map);
+            expect(Array.from(r.suits.keys())).to.include('Diamonds');
+            expect(r.suits.get('Diamonds')).to.be.instanceof(Array).to.include(R('Nine').value);
         })
     })
 })

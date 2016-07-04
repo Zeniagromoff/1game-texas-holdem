f5766703a3df2fe336e1d2fa16f04722d6cb4d43
鄭脈龍 <hausenism@hotmail.com>
2016-07-04 21:45:20 +0800

    feat(rule): add check for Ranks (Quad,FullHouse,Set,TwoPairs,Pair,High)

--- src/rule.js
+++ src/rule.js
@@ -1,9 +1,10 @@
 'use strict'
 const straight = (1 << 5) + (1 << 4) + (1 << 3) + (1 << 2) + (1 << 1);
+const v$ = 0, c$ = 1;
 
-function _count(input) {
-    var res = new Object(), sts = res.suits = new Map(), rks = res.ranks = new Map();
-    res.opps = 7 - input.length;
+function _count(input, output) {
+    var sts = output.suits = new Map(), rks = output.ranks = new Map();
+    output.opps = 7 - input.length;
     for (var i = 0; i < input.length; i++) {
         var card = input[i];
         var suit = card.suit, rValue = card.rank.value;
@@ -12,11 +13,24 @@ function _count(input) {
         if (rks.has(rValue)) { rks.set(rValue, rks.get(rValue) + 1); }
         else { rks.set(rValue, 1); }
     }
-    return res;
+    output.suits.forEach(function (rankValueArr, suit, map) {
+        if (rankValueArr.length >= 5) {
+            output.suit = suit;
+            _countStraightHigh(rankValueArr, output);
+            output.type = (output.type || '') + 'Flush';
+        }
+    })
+    if (!output.type) {
+        _countStraightHigh(Array.from(output.ranks.keys()), output);
+        if (!output.type) {
+            _countKinds(Array.from(output.ranks.entries()), output);
+        }
+    }
+    return output;
 }
 
-function _countStraightHigh(rankValueArr) {
-    var c = 0, s = 0, res = new Object(), f, h = 0;
+function _countStraightHigh(rankValueArr, output) {
+    var c = 0, s = 0, f, h = 0;
     for (var i = 0; i < rankValueArr.length; i++) {
         var value = rankValueArr[i];
         s += (1 << value);
@@ -27,35 +41,56 @@ function _countStraightHigh(rankValueArr) {
         if ((s & straight) == straight) { f = c; }
         ++c; s = s >> 1;
     }
-    res.strHigh = f ? 5 + f : f;
-    res.high = h;
-    return res;
+    if (f) {
+        output.type = "Straight";
+        output.strHigh = 5 + f;
+    }
+    output.high = h;
+    return output;
+}
+
+function _countKinds(vcArr, output) {
+    vcArr.sort(function (a, b) {
+        // b is greater than a, then swap.
+        var count = b[c$] - a[c$];
+        if (count == 0) count = b[v$] - a[v$];
+        return count;
+    });
+    if (vcArr[0][c$] == 4) {
+        output.type = 'Quad';
+        output.value = vcArr.shift()[v$];
+        output.high = Math.max.apply(Math, vcArr.map(e => e[v$]));
+    } else if (vcArr[0][c$] == 3) {
+        output.value = vcArr.shift()[v$];
+        if (vcArr[0][c$] > 1) {
+            output.type = 'FullHouse';
+            output.high = vcArr[0][v$];
+        } else {
+            output.type = 'Set';
+            output.high = [vcArr[0][v$], vcArr[1][v$]];
+        }
+    } else if (vcArr[0][c$] == 2) {
+        if (vcArr[1][c$] == 2) {
+            output.type = 'TwoPairs';
+            output.value = [vcArr[0][v$], vcArr[1][v$]];
+            output.high = vcArr[2][v$];
+        } else {
+            output.type = 'Pair';
+            output.value = vcArr[0][v$];
+            output.high = [vcArr[1][v$], vcArr[2][v$], vcArr[3][v$]];
+        }
+    } else {
+        output.type = 'High';
+        output.value = vcArr.slice(0, 5).map(e => e[v$]);
+    }
+    return output;
 }
 
 function count(showhand, board) {
     var input = showhand.concat(board);
-    var res = _count(input);
-    res.suits.forEach(function (rankValueArr, suit, map) {
-        if (rankValueArr.length >= 5) {
-            res.suit = suit;
-            var r = _countStraightHigh(rankValueArr);
-            if (r.strHigh) {
-                res.type = 'Straight Flush';
-                res.high = r.strHigh;
-            } else {
-                res.type = 'Flush';
-                res.high = r.high;
-            }
-        }
-    })
-    if (!res.type) {
-        var r = _countStraightHigh(Array.from(res.ranks.keys()));
-        if (r.strHigh) {
-            res.type = 'Straight';
-            res.high = r.strHigh;
-        }
-    }
-    return res;
+    var output = new Object();
+    _count(input, output);
+    return output;
 }
 
 module.exports = { count: count };
--- test/rule.test.js
+++ test/rule.test.js
@@ -10,9 +10,9 @@ describe('rule', function () {
             var showhand = [$('Diamonds', 'Seven'), $('Diamonds', 'Ten')];
             var board = [$('Diamonds', 'Eight'), $('Hearts', 'Four'), $('Diamonds', 'Nine'), $('Clubs', 'Two'), $('Diamonds', 'Six')];
             var r = count(showhand, board);
-            expect(r).to.have.property('type', 'Straight Flush');
+            expect(r).to.have.property('type', 'StraightFlush');
             expect(r).to.have.property('suit', 'Diamonds');
-            expect(r).to.have.property('high', R('Ten').value);
+            expect(r).to.have.property('strHigh', R('Ten').value);
         })
 
         it('should be a Flush with Ace High', function () {
@@ -32,13 +32,94 @@ describe('rule', function () {
             expect(r).to.have.property('high', R('Jack').value);
         })
 
-        it('should be implemented later', function () {
-            var showhand = [$('Spades', 'Seven'), $('Clubs', 'Eight')];
-            var board = [$('Hearts', 'Four'), $('Diamonds', 'Nine'), $('Spades', 'Eight'), $('Hearts', 'Nine'), $('Diamonds', 'Four')];
+        it('should be a Quad of Queen with Ace High', function () {
+            var showhand = [$('Spades', 'Queen'), $('Clubs', 'Queen')];
+            var board = [$('Hearts', 'Queen'), $('Diamonds', 'Nine'), $('Spades', 'Ace'), $('Hearts', 'Nine'), $('Diamonds', 'Queen')];
             var r = count(showhand, board);
-            expect(r.suits).to.be.instanceof(Map);
-            expect(Array.from(r.suits.keys())).to.include('Diamonds');
-            expect(r.suits.get('Diamonds')).to.be.instanceof(Array).to.include(R('Nine').value);
+            expect(r).to.have.property('type', 'Quad');
+            expect(r).to.have.property('value', R('Queen').value);
+            expect(r).to.have.property('high', R('Ace').value);
+        })
+
+        it('should be a Quad of Two with Ten High', function () {
+            var showhand = [$('Spades', 'Two'), $('Clubs', 'Two')];
+            var board = [$('Hearts', 'Two'), $('Diamonds', 'Nine'), $('Spades', 'Ten'), $('Hearts', 'Six'), $('Diamonds', 'Two')];
+            var r = count(showhand, board);
+            expect(r).to.have.property('type', 'Quad');
+            expect(r).to.have.property('value', R('Two').value);
+            expect(r).to.have.property('high', R('Ten').value);
+        })
+
+        it('should be a FullHouse of Eight with Five High', function () {
+            var showhand = [$('Spades', 'Eight'), $('Clubs', 'Two')];
+            var board = [$('Hearts', 'Five'), $('Diamonds', 'Five'), $('Clubs', 'Five'), $('Hearts', 'Eight'), $('Diamonds', 'Eight')];
+            var r = count(showhand, board);
+            expect(r).to.have.property('type', 'FullHouse');
+            expect(r).to.have.property('value', R('Eight').value);
+            expect(r).to.have.property('high', R('Five').value);
+        })
+
+        it('should be a FullHouse of Five with Ace High', function () {
+            var showhand = [$('Spades', 'Eight'), $('Diamonds', 'Ace')];
+            var board = [$('Hearts', 'Five'), $('Clubs', 'Ace'), $('Diamonds', 'Five'), $('Clubs', 'Five'), $('Hearts', 'Eight')];
+            var r = count(showhand, board);
+            expect(r).to.have.property('type', 'FullHouse');
+            expect(r).to.have.property('value', R('Five').value);
+            expect(r).to.have.property('high', R('Ace').value);
+        })
+
+        it('should be a FullHouse of Five with Eight High', function () {
+            var showhand = [$('Spades', 'Eight'), $('Diamonds', 'Ace')];
+            var board = [$('Hearts', 'Five'), $('Clubs', 'Two'), $('Diamonds', 'Five'), $('Clubs', 'Five'), $('Hearts', 'Eight')];
+            var r = count(showhand, board);
+            expect(r).to.have.property('type', 'FullHouse');
+            expect(r).to.have.property('value', R('Five').value);
+            expect(r).to.have.property('high', R('Eight').value);
+        })
+
+        it('should be a Set of Nine with King & Eight High', function () {
+            var showhand = [$('Spades', 'Nine'), $('Diamonds', 'King')];
+            var board = [$('Hearts', 'Nine'), $('Clubs', 'Two'), $('Diamonds', 'Nine'), $('Clubs', 'Five'), $('Hearts', 'Eight')];
+            var r = count(showhand, board);
+            expect(r).to.have.property('type', 'Set');
+            expect(r).to.have.property('value', R('Nine').value);
+            expect(r.high).to.be.instanceof(Array).to.have.lengthOf(2).to.include.members([R('King').value, R('Eight').value]);
+        })
+
+        it('should be a TwoPairs of King & Queen with Jack High', function () {
+            var showhand = [$('Spades', 'Queen'), $('Diamonds', 'King')];
+            var board = [$('Hearts', 'King'), $('Clubs', 'Jack'), $('Diamonds', 'Queen'), $('Clubs', 'Five'), $('Hearts', 'Jack')];
+            var r = count(showhand, board);
+            expect(r).to.have.property('type', 'TwoPairs');
+            expect(r.value).to.be.instanceof(Array).to.have.lengthOf(2).to.include.members([R('Queen').value, R('King').value]);
+            expect(r).to.have.property('high', R('Jack').value);
+        })
+
+        it('should be a TwoPairs of King & Jack with Five High', function () {
+            var showhand = [$('Spades', 'Two'), $('Diamonds', 'King')];
+            var board = [$('Hearts', 'King'), $('Clubs', 'Jack'), $('Diamonds', 'Four'), $('Clubs', 'Five'), $('Hearts', 'Jack')];
+            var r = count(showhand, board);
+            expect(r).to.have.property('type', 'TwoPairs');
+            expect(r.value).to.be.instanceof(Array).to.have.lengthOf(2).to.include.members([R('Jack').value, R('King').value]);
+            expect(r).to.have.property('high', R('Five').value);
+        })
+
+        it('should be a Pair of King with Jack & Seven & Five High', function () {
+            var showhand = [$('Spades', 'Two'), $('Diamonds', 'King')];
+            var board = [$('Hearts', 'King'), $('Clubs', 'Jack'), $('Diamonds', 'Four'), $('Clubs', 'Five'), $('Hearts', 'Seven')];
+            var r = count(showhand, board);
+            expect(r).to.have.property('type', 'Pair');
+            expect(r).to.have.property('value', R('King').value);
+            expect(r.high).to.be.instanceof(Array).to.have.lengthOf(3).to.include.members([R('Seven').value, R('Five').value, R('Jack').value]);
+        })
+
+        it('should be a High of Ace & King & Jack & Seven & Six High', function () {
+            var showhand = [$('Spades', 'Ace'), $('Diamonds', 'King')];
+            var board = [$('Hearts', 'Six'), $('Clubs', 'Jack'), $('Diamonds', 'Two'), $('Clubs', 'Five'), $('Hearts', 'Seven')];
+            var r = count(showhand, board);
+            expect(r).to.have.property('type', 'High');
+            expect(r.value).to.be.instanceof(Array).to.have.lengthOf(5)
+                .to.include.members([R('King').value, R('Ace').value, R('Jack').value, R('Six').value, R('Seven').value]);
         })
     })
 })
